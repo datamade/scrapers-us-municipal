a16df67df02edd60ed9f5f97ede3d3b7fd909e43
hancush <hannah.cushman@gmail.com>
2020-02-04 14:06:44 -0600

    Use the power of scripting to make code more readable, less redundant

--- scripts/lametro/fast-full-bill-scrape.sh
+++ scripts/lametro/fast-full-bill-scrape.sh
@@ -1,6 +1,9 @@
 #/bin/sh
+set -e
 
-(cd $APPDIR && \
-    $PUPADIR update --datadir=/cache/bills/_data/ lametro --scrape bills --window=0 --rpm=0 && \
-    $PUPADIR update --datadir=/cache/bills/_data/ lametro --import && \
-    SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa --datadir=/cache/bills/_data/ update lametro --import)
+exec 2>&1
+
+cd $APPDIR
+$PUPADIR update --datadir=/cache/bills/_data/ lametro --scrape bills --window=0 --rpm=0
+$PUPADIR update --datadir=/cache/bills/_data/ lametro --import
+SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa --datadir=/cache/bills/_data/ update lametro --import
--- scripts/lametro/full-scrape.sh
+++ scripts/lametro/full-scrape.sh
@@ -1,9 +1,17 @@
 #/bin/sh
+set -e
 
-(cd $APPDIR && \
-    $PUPADIR update lametro --scrape && \
-    $PUPADIR update lametro --import && \
-    SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa update lametro --import && \
-    $PUPADIR update lametro --scrape bills window=0 && \
-    $PUPADIR update lametro --import && \
-    SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa update lametro --import)
+exec 2>&1
+
+cd $APPDIR
+
+# Bills are windowed to 3 days by default. Scrape all people, all events, and
+# windowed bills.
+$PUPADIR update lametro --scrape
+$PUPADIR update lametro --import
+SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa update lametro --import
+
+# Scrape all bills.
+$PUPADIR update lametro --scrape bills window=0
+$PUPADIR update lametro --import
+SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa update lametro --import
