2e1fe56d13a716e2fac149069f708376378d9744
hancush <hannah.cushman@gmail.com>
2020-02-04 14:51:12 -0600

    Only scrape

--- scripts/lametro/windowed-bill-scrape.sh
+++ scripts/lametro/windowed-bill-scrape.sh
@@ -4,6 +4,6 @@ set -e
 exec 2>&1
 
 cd $APPDIR
-$PUPADIR update --datadir=/cache/bills/_data/ lametro bills window=$WINDOW
+$PUPADIR update --datadir=/cache/bills/_data/ lametro --scrape bills window=$WINDOW
 $PUPADIR update --datadir=/cache/bills/_data/ lametro --import
 SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa update --datadir=/cache/bills/_data/ lametro --import
