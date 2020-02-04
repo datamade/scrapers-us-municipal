e99390edb2d720a9760d5f6d8374563681385e9b
hancush <hannah.cushman@gmail.com>
2020-02-04 14:06:44 -0600

    Use the power of scripting to make code more readable, less redundant

--- scripts/lametro/windowed-bill-scrape.sh
+++ scripts/lametro/windowed-bill-scrape.sh
@@ -1,6 +1,9 @@
 #/bin/sh
+set -e
 
-(cd $APPDIR && \
-    $PUPADIR update --datadir=/cache/bills/_data/ lametro bills window=$WINDOW && \
-    $PUPADIR update --datadir=/cache/bills/_data/ lametro --import && \
-    SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa update --datadir=/cache/bills/_data/ lametro --import)
+exec 2>&1
+
+cd $APPDIR
+$PUPADIR update --datadir=/cache/bills/_data/ lametro bills window=$WINDOW
+$PUPADIR update --datadir=/cache/bills/_data/ lametro --import
+SHARED_DB=True DATABASE_URL=postgis://datamade@lametro-upgrade.datamade.us/lametro_staging pupa update --datadir=/cache/bills/_data/ lametro --import
