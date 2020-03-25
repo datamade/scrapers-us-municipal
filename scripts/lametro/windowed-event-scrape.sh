d71f3463d2c8eeac8e84f492e60324e01d6b37eb
hancush <hannah.cushman@gmail.com>
2020-03-25 11:31:31 -0500

    Revert "Only populate staging database once a day"
    This reverts commit ed3c481c5a6bfdd307f2a0977b3cd936340ad51b.

--- scripts/lametro/windowed-event-scrape.sh
+++ scripts/lametro/windowed-event-scrape.sh
@@ -9,3 +9,4 @@ $PUPADIR update --datadir=/cache/events/_data/ lametro --scrape events window=$W
 # $PUPADIR update --datadir=/cache/events/_data/ lametro --import
 # TODO: Update database URL before production deployment on remote server.
 SHARED_DB=True DATABASE_URL=postgis://datamade@localhost/lametro $PUPADIR update --datadir=/cache/events/_data/ lametro --import
+SHARED_DB=True DATABASE_URL=postgis://datamade@localhost/lametro_staging $PUPADIR update --datadir=/cache/events/_data/ lametro --import
