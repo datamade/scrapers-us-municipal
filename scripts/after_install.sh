12a45dc3c8a5e08da9c0cccdf3f121beace4754c
hancush <hannah.cushman@gmail.com>
2020-03-31 10:40:38 -0500

    Reorder script for legibility

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -16,15 +16,14 @@ chown -R datamade.www-data $VIRTUALENV
 $VIRTUALENV/bin/pip install --upgrade pip
 $VIRTUALENV/bin/pip install -r $APP_PATH/requirements.txt --upgrade
 
-# Move crontask to correct place, and assign correct ownership and permissions.
+# Move crontasks to correct place, and assign correct ownership and permissions.
 mv $APP_PATH/scripts/scrapers-us-municipal-crontask /etc/cron.d/scrapers-us-municipal-crontask
 chown root.root /etc/cron.d/scrapers-us-municipal-crontask
 chmod 644 /etc/cron.d/scrapers-us-municipal-crontask
 
-# Run Migrations
-cd $APP_PATH && DJANGO_SETTINGS_MODULE=pupa.settings $VIRTUALENV/bin/django-admin migrate
-
-# Move crontask to correct place, and assign correct ownership and permissions.
 mv /home/datamade/scrapers-us-municipal/scripts/la-metro-crontask /etc/cron.d/la-metro-crontask
 chown root.root /etc/cron.d/la-metro-crontask
 chmod 644 /etc/cron.d/la-metro-crontask
+
+# Run Migrations
+cd $APP_PATH && DJANGO_SETTINGS_MODULE=pupa.settings $VIRTUALENV/bin/django-admin migrate
