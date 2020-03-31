d4e058ebd182d86b3f00b05fe96a18306e42d1e3
hancush <hannah.cushman@gmail.com>
2020-03-31 10:39:11 -0500

    Move master cron and run migrations on deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -17,14 +17,12 @@ $VIRTUALENV/bin/pip install --upgrade pip
 $VIRTUALENV/bin/pip install -r $APP_PATH/requirements.txt --upgrade
 
 # Move crontask to correct place, and assign correct ownership and permissions.
-# TODO: Uncomment this before production deployment.
-# mv $APP_PATH/scripts/scrapers-us-municipal-crontask /etc/cron.d/scrapers-us-municipal-crontask
-# chown root.root /etc/cron.d/scrapers-us-municipal-crontask
-# chmod 644 /etc/cron.d/scrapers-us-municipal-crontask
+mv $APP_PATH/scripts/scrapers-us-municipal-crontask /etc/cron.d/scrapers-us-municipal-crontask
+chown root.root /etc/cron.d/scrapers-us-municipal-crontask
+chmod 644 /etc/cron.d/scrapers-us-municipal-crontask
 
 # Run Migrations
-# TODO: Uncomment this before production deployment.
-# cd $APP_PATH && DJANGO_SETTINGS_MODULE=pupa.settings $VIRTUALENV/bin/django-admin migrate
+cd $APP_PATH && DJANGO_SETTINGS_MODULE=pupa.settings $VIRTUALENV/bin/django-admin migrate
 
 # Move crontask to correct place, and assign correct ownership and permissions.
 mv /home/datamade/scrapers-us-municipal/scripts/la-metro-crontask /etc/cron.d/la-metro-crontask
