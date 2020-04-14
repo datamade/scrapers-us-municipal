f3f4b930847cc556be8a9ca3f057ce7826ac5ece
hancush <hannah.cushman@gmail.com>
2020-04-14 14:08:23 -0500

    Use the right data directory 🙃

--- scripts/lametro/person-scrape.sh
+++ scripts/lametro/person-scrape.sh
@@ -4,6 +4,6 @@ set -e
 exec 2>&1
 
 cd $APPDIR
-$PUPADIR update --datadir=/cache/events/_data/ lametro --scrape people
-SHARED_DB=True DATABASE_URL=postgis://datamade@3.93.9.229/lametro $PUPADIR update --datadir=/cache/events/_data/ lametro --import people
-$PUPADIR update --datadir=/cache/events/_data/ lametro --import people
+$PUPADIR update --datadir=/cache/people/_data/ lametro --scrape people
+SHARED_DB=True DATABASE_URL=postgis://datamade@3.93.9.229/lametro $PUPADIR update --datadir=/cache/people/_data/ lametro --import people
+$PUPADIR update --datadir=/cache/people/_data/ lametro --import people
