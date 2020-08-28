b32a0bcc4d44a5a2d28bdcf30acc982a2e3485a2
Forest Gregg <fgregg@uchicago.edu>
2020-08-28 14:54:11 -0400

    do not require SHARED_DB as env variable since wealways need it

--- scripts/lametro/fast-full-bill-scrape.sh
+++ scripts/lametro/fast-full-bill-scrape.sh
@@ -5,5 +5,5 @@ exec 2>&1
 
 cd $APPDIR
 $PUPADIR update --datadir=/cache/bills/_data/ lametro --scrape bills window=0 --rpm=0
-SHARED_DB=True DATABASE_URL=postgis://datamade@3.93.9.229/lametro $PUPADIR update --datadir=/cache/bills/_data/ lametro --import
+DATABASE_URL=postgis://datamade@3.93.9.229/lametro $PUPADIR update --datadir=/cache/bills/_data/ lametro --import
 $PUPADIR update --datadir=/cache/bills/_data/ lametro --import
--- scripts/lametro/windowed-event-scrape.sh
+++ scripts/lametro/windowed-event-scrape.sh
@@ -5,5 +5,5 @@ exec 2>&1
 
 cd $APPDIR
 $PUPADIR update --datadir=/cache/events/_data/ lametro --scrape events window=$WINDOW
-SHARED_DB=True DATABASE_URL=postgis://datamade@3.93.9.229/lametro $PUPADIR update --datadir=/cache/events/_data/ lametro --import
+DATABASE_URL=postgis://datamade@3.93.9.229/lametro $PUPADIR update --datadir=/cache/events/_data/ lametro --import
 $PUPADIR update --datadir=/cache/events/_data/ lametro --import
